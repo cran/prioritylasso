138a56151748d1885f68267c204ea171898d7f66
Roman Hornung <hornung@ibe.med.uni-muenchen.de>
2023-03-28 13:40:02 +0000

    version 0.3.0

---
+++ inst/doc/explanation_blockwise_missing.R
@@ -0,0 +1,15 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ---- complete-cases, fig.cap = "Algorithm 1", echo = FALSE-------------------
+knitr::include_graphics("algorithm_1.PNG")
+
+## ---- available-cases, fig.cap = "Algorithm 2", echo = FALSE------------------
+knitr::include_graphics("algorithm_2.PNG")
+
+## ---- findallsuitableobservations, fig.cap = "Algorithm 3", echo = FALSE------
+knitr::include_graphics("algorithm_3.PNG")
+
